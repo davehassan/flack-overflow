47c9d977b8a2042343860ec4eb8f1a69632c73ab
App Academy Student <appacademy@Apps-Mac-mini-31.local>
2015-11-17 11:33:32 -0500

    Finish auth maybe finally

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save!
+      sign_in!(@user)
       redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   after_initialize :ensure_session_token
   attr_reader :password
   validates :username, :password_digest, :session_token, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   def self.find_by_credentials(username, password)
+    byebug
     user = User.find_by_username(username)
 
     user && user.is_password?(password) ? user : nil
